bd283e676f5b53fd1017dade721b39aef0c0dde5
Aditya092 <92735842+Aditya092@users.noreply.github.com>
2023-03-07 13:49:44 -0500

    updated code for step 7 (still not finished)

--- site.js
+++ site.js
@@ -41,11 +41,51 @@ const vue_app = Vue.createApp({
       }
     },
       methods: {
-            handleClick: getMonthText(dateArray) {
-                  alert(text)
-            }
+              getMonthFormat(dateArray){
+                  let month = "";
+                  switch(dateArray[1]) {
+                        case 1: 
+                              month = "January";
+                              break;
+                        case 2: 
+                              month = "Febuary";
+                              break;
+                        case 3: 
+                              month = "March";
+                              break;
+                        case 4: 
+                              month = "April";
+                              break;
+                        case 5: 
+                              month = "May";
+                              break;
+                        case 6: 
+                              month = "June";
+                              break;
+                        case 7: 
+                              month = "July";
+                              break;
+                        case 8: 
+                              month = "August";
+                              break;
+                        case 9: 
+                              month = "September";
+                              break;
+                        case 10: 
+                              month = "October";
+                              break;
+                        case 11: 
+                              month = "November";
+                              break;
+                        case 12: 
+                              month = "December";
+                              break;
+                  }
+                   return + ", " + dateArray[2]  + ", " + dateArray[0] + ", "    
+            },
+            },
 
       }
-})
+)
 
 vue_app.mount("#vue_app")
